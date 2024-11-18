21d1a7250c8fdfd27e1ba171f48119d7a7ef89a6
Md.Dillshadul Islam <mddillshadul98@gmail.com>
2024-11-18 10:21:35 +0600

    added homepage link for hosting

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/autism_in_toddlers/" element={<HomePage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/contact-us" element={<ContactUs />} />
